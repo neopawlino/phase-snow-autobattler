05ad4444c0608738a682a59ed7eae473f9530fc8
Neo Pawlino <neopawlino@proton.me>
2025-01-05 14:46:25 -0800

    StatusIcon: Add tooltip text

--- status_effects/status_icon.gd
+++ status_effects/status_icon.gd
@@ -13,12 +13,14 @@ var value : int = 0:
 		value = val
 		update_value(val)
 
+var status : StatusEffect.StatusId
 
 static var status_icon_scene : PackedScene = preload("res://status_effects/StatusIcon.tscn")
 
 
 static func make_status_icon(status : StatusEffect.StatusId, value : int = 0) -> StatusIcon:
 	var status_icon : StatusIcon = status_icon_scene.instantiate()
+	status_icon.status = status
 	status_icon.icon.texture = StatusIconLib.get_icon_for_status(status)
 	status_icon.value = value
 	return status_icon
@@ -28,3 +30,4 @@ func update_value(val: int):
 	self.visible = val != 0
 	value_label.text = str(val)
 	value_label.add_theme_color_override(&"font_color", pos_color if val > 0 else neg_color)
+	self.tooltip_text = StatusIconLib.get_tooltip_text_for_status(self.status) % val
--- status_effects/status_icon_definition.gd
+++ status_effects/status_icon_definition.gd
@@ -4,3 +4,4 @@ class_name StatusIconDefinition
 
 @export var status : StatusEffect.StatusId
 @export var icon : Texture2D
+@export var tooltip_text : String
