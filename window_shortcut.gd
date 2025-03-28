c3f3980ae4d69d19b179de9dd0437dde8dcc40b4
Neo Pawlino <neopawlino@proton.me>
2025-03-28 11:55:39 -0700

    KBS: UI tweaks

--- window_shortcut.gd
+++ window_shortcut.gd
@@ -2,9 +2,17 @@ extends Button
 
 @export var window : Window
 
+var default_size : Vector2
+var default_pos : Vector2
+
 func _ready() -> void:
 	self.pressed.connect(on_pressed)
+	self.default_size = window.size
+	self.default_pos = window.position
 
 
 func on_pressed():
-	window.visible = true
+	if not window.visible:
+		window.size = self.default_size
+		window.position = self.default_pos
+		window.visible = true
