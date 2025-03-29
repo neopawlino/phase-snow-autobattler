fa811be2aa45f9751f2bdae2bea62bdd3e7f3ade
Neo Pawlino <neopawlino@proton.me>
2025-03-28 17:02:25 -0700

    Minor: Remove debug print

--- viewport_push_input.gd
+++ viewport_push_input.gd
@@ -1,10 +1,7 @@
 extends Node
 
 @export var viewport : SubViewport
-@export var test_debug : bool
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if test_debug and event.is_action_pressed("click"):
-		print('foo')
 	viewport.push_input(event)
