e2ef9c333cf4e3777cb602447e1dd86a274c14fc
Neo Pawlino <neopawlino@proton.me>
2025-04-04 11:32:49 -0700

    Theme: Button adjustments

--- start_streaming_button.gd
+++ start_streaming_button.gd
@@ -4,3 +4,6 @@ func _ready() -> void:
 	self.pressed.connect(func():
 		GlobalSignals.stream_anim_started.emit()
 	)
+	GlobalSignals.stream_results_confirmed.connect(func():
+		self.set_pressed_no_signal(false)
+	)
