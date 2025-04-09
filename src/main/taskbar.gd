06621a17489b5ea25ef3002e3d642e386dc6ace1
Neo Pawlino <neopawlino@proton.me>
2025-04-09 12:04:22 -0700

    Desktop: Add settings window

--- src/main/taskbar.gd
+++ src/main/taskbar.gd
@@ -2,11 +2,38 @@ extends Control
 
 var initial_position : Vector2
 
+@export var settings_window : Window
+
+@export var settings_button : Button
+@export var quit_button : Button
+
+@export var start_menu : Control
+
+var settings_default_size
+var settings_default_pos
+
 
 func _ready() -> void:
 	self.initial_position = self.global_position
+
+	self.settings_default_size = settings_window.size
+	self.settings_default_pos = settings_window.position
+
 	GlobalSignals.stream_anim_started.connect(hide_anim)
 	GlobalSignals.stream_results_confirmed.connect(show_anim)
+	self.settings_button.pressed.connect(on_settings_button_pressed)
+
+	# Hide quit button on web export
+	self.quit_button.visible = not OS.has_feature("web")
+
+
+func on_settings_button_pressed():
+	if not settings_window.visible:
+		settings_window.size = self.settings_default_size
+		settings_window.position = self.settings_default_pos
+		settings_window.visible = true
+	settings_window.grab_focus()
+	start_menu.hide()
 
 
 func hide_anim():
