e551bb01153afe6589e9e93e01d3fa5c24f9210c
Neo Pawlino <neopawlino@proton.me>
2025-04-09 09:55:07 -0700

    UI: Adjust stream speed controls

--- src/ui/speed_button.gd
+++ src/ui/speed_button.gd
@@ -1,4 +1,8 @@
-extends Button
+extends Node
+
+@export var decrease_button : Button
+@export var increase_button : Button
+@export var speed_label : Label
 
 
 var speed_options : Array[int] = [
@@ -14,12 +18,17 @@ const SETTINGS_KEY : String = "stream_speed"
 
 
 func _ready() -> void:
-	self.pressed.connect(on_pressed)
+	self.increase_button.pressed.connect(increase_speed)
+	self.decrease_button.pressed.connect(decrease_speed)
 	self.speed_index = Settings.get_value(SETTINGS_KEY, 0)
 	GlobalSignals.stream_started.connect(func():
+		self.increase_button.disabled = false
+		self.decrease_button.disabled = false
 		self.set_speed(speed_index)
 	)
 	GlobalSignals.stream_ended.connect(func():
+		self.increase_button.disabled = true
+		self.decrease_button.disabled = true
 		self.set_speed(0)
 	)
 
@@ -30,12 +39,24 @@ func on_pressed() -> void:
 	self.save_speed(speed_index)
 
 
+func increase_speed() -> void:
+	self.speed_index = mini(speed_index + 1, len(speed_options) - 1)
+	self.set_speed(speed_index)
+	self.save_speed(speed_index)
+
+
+func decrease_speed() -> void:
+	self.speed_index = maxi(speed_index - 1, 0)
+	self.set_speed(speed_index)
+	self.save_speed(speed_index)
+
+
 func set_speed(i : int) -> void:
 	var speed := speed_options[i]
 	var base_physics_tps : int = ProjectSettings.get_setting("physics/common/physics_ticks_per_second")
 	Engine.time_scale = float(speed)
 	Engine.physics_ticks_per_second = base_physics_tps * speed
-	self.text = "Speed: %dx" % speed
+	self.speed_label.text = "Speed: %dx" % speed
 
 
 func save_speed(i : int) -> void:
