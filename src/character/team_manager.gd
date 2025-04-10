f61fd81ad20cf9e62e27fa104443de76dc74b4f7
Neo Pawlino <neopawlino@proton.me>
2025-04-10 11:47:28 -0700

    Game: Start with random talent

--- src/character/team_manager.gd
+++ src/character/team_manager.gd
@@ -2,12 +2,7 @@ extends Node
 
 class_name TeamManager
 
-@export var max_slots : int = 6
-
-@export var initial_player_team : Array[CharacterDefinition]
 @export var enemy_layouts : EnemyLayouts
-
-@export var test_character : CharacterDefinition
 
 var character_scene : PackedScene = preload("res://src/character/character.tscn")
 var character_slot_scene : PackedScene = preload("res://src/item/character_slot.tscn")
@@ -17,20 +12,16 @@ func _ready():
 	GameState.wins_needed = enemy_layouts.wins_needed
 	GameState.player_hp = enemy_layouts.initial_lives
 
-	load_initial_teams()
+	call_deferred(&"load_initial_team")
 
 
-func load_initial_teams():
-	var i : int = 0
-	for char_def in initial_player_team:
-		var char : Character = character_scene.instantiate()
-		var slot : Slot = GameState.main_slots.slots[i]
-		char.load_from_character_definition(char_def)
-		char.team = Character.Team.PLAYER
-		char.drag_component.cur_slot = slot
-		char.drag_component.draggable = true
+func load_initial_team():
+	add_char(RandomUtil.all_character_definitions.pick_random())
 
-		char.global_position = slot.global_position
-		slot.slot_obj = char
-		slot.add_child(char)
-		i += 1
+
+func add_char(char_def : CharacterDefinition):
+	var char : Character = character_scene.instantiate()
+	char.load_from_character_definition(char_def)
+	char.team = Character.Team.PLAYER
+	self.add_child(char)
+	GameState.main_slots.push_char(char)
